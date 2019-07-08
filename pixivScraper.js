4bee0c1c6f88fdb7dddd52584cb0cf9bac404b7e
Edward Carlson <plupiman@outlook.com>
2019-07-08 11:32:54 +0900

    Working state but still says 404

--- pixivScraper.js
+++ pixivScraper.js
@@ -14,6 +14,7 @@ const maxPicPerPage = 48;
 const seeAllButtonPos = 7;
 const expandStateNextPos = 62;
 const expandStateTryPos = 80;
+let testCounter = 0;
 
 
 async function logIn(browser, page){
@@ -59,55 +60,73 @@ async function logIn(browser, page){
 	console.log('Session has been saved to ' + cookiesFilePath);
 }
 
+
+
 //Steps through the artists pages, going to the individual art pages
 async function stepThroughArtist(browser, page, artistID, filter, fArgs){
 	var acceptedPics = [];
 	var baseArtistURL = "https://www.pixiv.net/member_illust.php?id=" + artistID + "&type=illust&p="; //base url for picture pages of an artist 
 	var counter = 1; //page counter
 	await page.goto(baseArtistURL + counter.toString());
-	console.log(page.url());
-	page.on('console', consoleObj => console.log(consoleObj.text()));
+	
 	var hasNextPage = true;
 	await page.waitForFunction('document.querySelector("#root")');
+	await page.screenshot({path: 'arrivedAtArtistPage.png', fullPage: true});
 	while (hasNextPage){
-		page.evaluate((maxPicPerPage, acceptedPics) => {
+		let tempPicStorage =  //the list of image page URLS on the artists page
+		await page.evaluate((maxPicPerPage, hasNextPage) => { 
 			var container = document.querySelector("#root");
 			var hits = container.querySelectorAll("li > div > a");
+			let tempPicStorage = [];
 			hasNextPage = (hits.length == maxPicPerPage); 
 			for (i = 0; i < hits.length ; i++){ // go through every image on the page 
-				if (checkImage(browser, page, filter, hits[i].href, fArgs)){ //check the image page and add if it passes
-					acceptedPics.push(hits[i].href);
-				}
+				tempPicStorage.push(hits[i].href);
 			}
-		}, maxPicPerPage, acceptedPics);
+			return tempPicStorage;
+		}, maxPicPerPage, hasNextPage);
+		for (let i = 0 ; i < tempPicStorage.length; i++){
+			if (await checkImage(browser, page, filter, tempPicStorage[i], fArgs)){ //if pic was accepted
+				acceptedPics.push(tempPicStorage[i]);
+			}
+		}
 		counter++;
 		await page.goto(baseArtistURL + counter.toString());
 		hasNextPage = false; //remove this when actually testing<----------------------
 	}
+	console.log("accepted pics: " + acceptedPics);
 }
 
 //Checks the specific page and prepares the filters to check if it passes the rule
 async function checkImage(browser, page, filter, imgPageURL, fArgs){
-	await page.goto(imgPageURL);
+	testCounter++;
+	console.log("entered checkImage");
+	await page.waitFor(2000);
+	let result = await page.goto(imgPageURL);
+	console.log("result: " + result.status());
 	await page.waitForFunction('document.querySelector("#root")');
 	//page.waitForNavigation({ waitUntil: 'networkidle0' });
 	await page.screenshot({path: 'preClick.png', fullPage: true});
-	page.on('console', consoleObj => console.log(consoleObj.text()));
-	const singlePiecePage = await page.evaluate(() => { 
-	console.log("button count: " + document.querySelectorAll("button").length);
-	return document.querySelectorAll("button").length == 42;}); //check if single or multi page
+	const singlePiecePage = await page.evaluate(() => { //check if single or multi page
+		console.log("singlepiece eval called");
+		console.log("buttons: " + document.querySelectorAll("button").length);
+		for (let i = 0 ; i < document.querySelectorAll("button").length ; i++){
+			if (document.querySelectorAll("button")[i].innerText == "See all"){
+				return false;
+			}
+		}
+		return true;}); 
 	console.log("singlePiecePage: " + singlePiecePage);
+	console.log(page.url());
 	if (!singlePiecePage){
 		console.log("not single pic page");
 		page.evaluate((seeAllButtonPos) => { document.querySelectorAll("button")[seeAllButtonPos].click(); }, seeAllButtonPos); //expands multi image pages
 		await page.waitFor(1000);
-		await page.screenshot({path: 'postClick1.png', fullPage: true});
-		page.evaluate((expandStateNextPos) => { document.querySelectorAll("button")[expandStateNextPos].click(); }, expandStateNextPos);
+		/*page.evaluate((expandStateNextPos) => { document.querySelectorAll("button")[expandStateNextPos].click(); }, expandStateNextPos);
 		await page.waitFor(1000);
-		await page.screenshot({path: 'postClick2.png', fullPage: true});
+		await page.screenshot({path: testCounter + '_2.png', fullPage: true});
 		page.evaluate((expandStateTryPos) => { document.querySelectorAll("button")[expandStateTryPos].click(); }, expandStateTryPos);
 		await page.waitFor(1000);
-		await page.screenshot({path: 'postClick3.png', fullPage: true});
+		await page.screenshot({path: testCounter + '_3.png', fullPage: true});*/
 	}
 
 	let finalFArgs = [];
@@ -119,10 +138,8 @@ async function checkImage(browser, page, filter, imgPageURL, fArgs){
 	}
 	
 	if(await filter.apply(this, finalFArgs)){//if it passes requirement
-		console.log("check image returning true");
 		return true;
 	} else {
-		console.log("check image returning false");
 		return false;
 	}
 	
@@ -146,19 +163,13 @@ async function biggerThanFilter(browser, page, isSingle, xReq, yReq){
 				aList[i].innerHTML.indexOf(imgId) != -1 &&
 				aList[i].innerHTML.indexOf("<img") == 0){
 					picsParsed++;
-					console.log(i);
-					console.log(hits[0].innerText);
-					console.log(aList[i].href);
-					console.log(aList[i].innerHTML);
 					let widthPointer = aList[i].innerHTML.indexOf("width=\"") + 7;
-					console.log("widthPointer: " + aList[i].innerHTML.charAt(widthPointer));
 					let widthStr = "";
 					while (!isNaN(aList[i].innerHTML.charAt(widthPointer))){ //while it points to a number
 						widthStr = widthStr + aList[i].innerHTML.charAt(widthPointer);
 						widthPointer++;
 					}
 					let heightPointer = aList[i].innerHTML.indexOf("height=\"") + 8;
-					console.log("heightPointer: " + aList[i].innerHTML.charAt(heightPointer));
 					let heightStr = "";
 					while (!isNaN(aList[i].innerHTML.charAt(heightPointer))){ //while it points to a number
 						heightStr = heightStr + aList[i].innerHTML.charAt(heightPointer);
@@ -174,7 +185,6 @@ async function biggerThanFilter(browser, page, isSingle, xReq, yReq){
 			}
 
 		}
-		console.log("parsed: " + picsParsed);
 		return false;
 	}, xReq, yReq, passesFilter, imgId);
 	console.log("hit end somehow");
@@ -188,9 +198,11 @@ async function hasTag(browser, page, isSingle, tag){
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await logIn(browser, page);
-	await page.screenshot({path: 'new.png', fullPage: true});
+	page.on('console', consoleObj => console.log(consoleObj.text()));
 	//const images = await page.$$eval('img', imgs => imgs.map(img => img.naturalWidth));
 	//console.log(images); //this gets the image width 
-	//stepThroughArtist(browser, page, 2087042);
-	checkImage(browser, page, biggerThanFilter, "https://www.pixiv.net/member_illust.php?mode=medium&illust_id=75165045", [10000,1] );
+	//await checkImage(browser, page, biggerThanFilter, "https://www.pixiv.net/member_illust.php?mode=medium&illust_id=75457374", [1,1] );
+	await stepThroughArtist(browser, page, 24218478, biggerThanFilter, [1779, 1000]);
+	console.log("end");
+	await browser.close();
 })();
